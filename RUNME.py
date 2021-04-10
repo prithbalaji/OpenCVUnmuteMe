6b7accca38bed6afa875cdd83074052d93b9e093
danny88881 <35647672+danny88881@users.noreply.github.com>
2021-04-10 18:03:55 -0500

    Change read_data to function

--- RUNME.py
+++ RUNME.py
@@ -6,6 +6,10 @@
 
 import tkinter as tk
 import mic_control as mic
+import read_data as mouthd
+
+#webcam_connected = False
+webcam_poll_delay = 50
 
 app_enable = True
 muted = False #case where muted starts as true?
@@ -15,8 +19,6 @@ c = 'green'
 root= tk.Tk()
 canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
-
-
 
 def switch():
     mic.mic_mute_toggle()
@@ -31,16 +33,19 @@ def switch():
         t = "unmuted"
         c= 'green'
     
-
 def enable_toggle():
     global app_enable
     app_enable = not app_enable
   
-
 button1 = tk.Button(text='Toggle Mute',command=switch, bg='brown',fg='white')
 canvas1.create_window(150, 150, window=button1)
 label1 = tk.Label(root, text= t, fg=c, font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 200, window=label1)
 
+def poll_webcam():
+    print(mouthd.get_mouth_state())
+    root.after(webcam_poll_delay, poll_webcam)  # reschedule event
 
+mouthd.initialize_capture()
+root.after(webcam_poll_delay, poll_webcam)
 root.mainloop()
