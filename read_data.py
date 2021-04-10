9c1810baf25dc20911c60df751542245b96927f8
danny88881 <35647672+danny88881@users.noreply.github.com>
2021-04-10 02:19:50 -0500

    Process webcam video to determine mouth state

--- read_data.py
+++ read_data.py
@@ -1,29 +1,85 @@
-import cv2 
+from imutils import face_utils
+import cv2
+import sys
+import dlib
+import imutils
 import numpy as np
 
-<<<<<<< HEAD
-cap = cv2.VideoCapture('Test.MOV')
-=======
-cap = cv2.VideoCapture('Test.MOV')#this is a local path will not run
->>>>>>> 3bf6ec98cf4bac70c95bac9e8e2c067f6e0464ef
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+#cap = cv2.VideoCapture('Test.MOV') #this is a local path will not run
+cap = cv2.VideoCapture(0) # takes input from default webcam
+
+OPEN = "open"
+CLOSED = "closed"
+
+top_mouth_range = [(61, 64)]#, (49, 54)] we only care about inner lips
+bottom_mouth_range = [(65, 68)] #(55, 60)]
+
+mouth_open_threshold = 8
 
 while True:
-        ret, frame  = cap.read()
+    ret, frame  = cap.read()
 
+    image = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('frame',frame)
+    rects = detector(gray, 1)
 
+    for (i, rect) in enumerate(rects):
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # convert dlib's rectangle to a OpenCV-style bounding box
+        # [i.e., (x, y, w, h)], then draw the face bounding box
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+        # show the face number
+        cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-<<<<<<< HEAD
+        # calculate average y positions of top and bottom lip
+        average_top_y = 0
+        total_points = 0
+        for (j, k) in top_mouth_range:
+            shape_sub_arr = shape[j:k]
+            for (x, y) in shape_sub_arr:
+                cv2.circle(image, (x, y), 1, (255, 0, 0), -1)
+                average_top_y += y
+                total_points += 1
+        average_top_y /= total_points
 
+        average_bottom_y = 0
+        total_points = 0
+        for (j, k) in bottom_mouth_range:
+            shape_sub_arr = shape[j:k]
+            for (x, y) in shape_sub_arr:
+                cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+                average_bottom_y += y
+                total_points += 1
+        average_bottom_y /= total_points
 
-=======
->>>>>>> 3bf6ec98cf4bac70c95bac9e8e2c067f6e0464ef
+        # set lip state based on threshold
+        face_state = CLOSED
+        if abs(average_top_y - average_bottom_y) > mouth_open_threshold:
+            face_state = OPEN
+
+        # show face state
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv2.putText(image, "Face State {}".format(face_state), (w + x - 100, h + y + 15),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+# show the output image with the face detections + facial landmarks
+
+    cv2.imshow('frame',image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 cap.release()
 cv2.destroyAllWindows()
 
