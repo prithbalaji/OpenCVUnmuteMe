e875623c7241ca0edb28717f331d8a103762efd1
prithbalaji <prithvibalaji16@gmail.com>
2021-04-10 15:02:08 -0700

    get threshold eye

--- eyetracker.py
+++ eyetracker.py
@@ -6,9 +6,12 @@ cap = cv2.VideoCapture("Test.MOV")
 
 while True:
     ret, frame = cap.read()
-    roi = frame[269: 795, 537:1416]
+    roi = frame[620: 810, 600:750]
+    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
-
+    _, threshold = cv2.threshold(gray_roi, 10 ,255, cv2.THRESH_BINARY_INV)
+    cv2.imshow("gray roi", gray_roi)
+    cv2.imshow("Threshold", threshold)
     cv2.imshow("Roi", roi)
     key = cv2.waitKey(30)
     if key == 27:
