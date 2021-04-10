3bf6ec98cf4bac70c95bac9e8e2c067f6e0464ef
Alan Wandke <awandke2@illinois.edu>
2021-04-09 21:43:17 -0500

    initial commit

---
+++ read_data.py
@@ -0,0 +1,20 @@
+import cv2 
+import numpy as np
+
+cap = cv2.VideoCapture('Test.MOV')#this is a local path will not run
+
+
+while True:
+        ret, frame  = cap.read()
+
+
+        cv2.imshow('frame',frame)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
