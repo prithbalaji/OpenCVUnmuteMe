ad06b9e92a987dbfe2d2012019044ffe10ee91bc
prithbalaji <prithvibalaji16@gmail.com>
2021-04-11 13:44:36 -0700

    eyes green when looking straight

--- eyetracker.py
+++ eyetracker.py
@@ -5,21 +5,21 @@ import cv2
 #FOR WINDOWS viewers, download opencv with the following command: pip install opencv-contrib-python
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 
 while True:
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5, minSize = (30,30), flags = cv2.CASCADE_SCALE_IMAGE)
 	
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
         roi_gray = gray[y:y+w, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+            cv2.rectangle(roi_color, (ey, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
     cv2.imshow('frame', frame)
 
