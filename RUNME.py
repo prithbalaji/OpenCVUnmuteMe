67b8cda325db482f04db992c946b92ae8ad0851d
Alan Wandke <awandke2@illinois.edu>
2021-04-10 21:51:47 -0500

    commit RUNME with enable/disable

--- RUNME.py
+++ RUNME.py
@@ -8,22 +8,23 @@ import tkinter as tk
 import mic_control as mic
 import read_data as mouthd
 
-#define global variables
+#define global variables 
 
 #webcam_connected = False
 webcam_poll_delay = 50
 
 app_enable = True
 muted = False #case where muted starts as true?
-t = "unmuted"
-c = 'green'
+
+dynamic_buttons = []
 
 #define functions 
 def switch():
+  t = "unmuted" #default value
+  c = "green" #default value
+  if (app_enable): #only do anything if enabled
     mic.mic_mute_toggle()
     global muted
-    global t
-    global c
     muted = not muted
     if (muted):
         t = "muted"
@@ -31,28 +32,43 @@ def switch():
     else:
         t = "unmuted"
         c= 'green'
-    
+  draw_mute_label(t,c)
+
 def enable_toggle():
     global app_enable
     app_enable = not app_enable
-    
+    if (app_enable):
+      e = "enabled"
+    else:
+      e = "disabled"
+    draw_enable_label(e)
+
 def poll_webcam():
     print(mouthd.get_mouth_state())
-    root.after(webcam_poll_delay, poll_webcam)  # reschedule event
-    
-#define application gui    
+    root.after(webcam_poll_delay, poll_webcam)  # reschedule event 
+
+def draw_mute_label(t,c):
+    mute_label = tk.Label(root, text= t, fg=c, font=('helvetica', 12, 'bold'))
+    canvas1.create_window(50, 150, window=mute_label)
+
+def draw_enable_label(e):
+    enable_label = tk.Label(root, text= e, fg="black", font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 150, window=enable_label)
+
+#define app
 root= tk.Tk()
 canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
+  
+#define widgets   
+mute_button = tk.Button(text='Toggle Mute',command=switch, bg='brown',fg='white')
+enable_button = tk.Button(text='enable app',command=enable_toggle, bg='brown',fg='white')
+draw_mute_label("unmuted","green")
+draw_enable_label("enabled")
 
-#create widgets  
-button1 = tk.Button(text='Toggle Mute',command=switch, bg='brown',fg='white')
-label1 = tk.Label(root, text= t, fg=c, font=('helvetica', 12, 'bold'))
-
-#add widgets to canvas
-canvas1.create_window(150, 150, window=button1)
-canvas1.create_window(150, 200, window=label1)
-
+#add widgets to canvas 
+canvas1.create_window(50, 50, window=mute_button)
+canvas1.create_window(150, 50, window=enable_button)
 
 
 #mouthd.initialize_capture()
