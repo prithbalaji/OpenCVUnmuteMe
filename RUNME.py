987a6b38707a3c4128151ce095b746ab5de86202
Alan Wandke <awandke2@illinois.edu>
2021-04-10 23:51:51 -0500

    application that watches your face

--- RUNME.py
+++ RUNME.py
@@ -13,18 +13,19 @@ import read_data as mouthd
 #webcam_connected = False
 webcam_poll_delay = 50
 
-app_enable = True
+app_enable = True #default is enabled
 muted = False #case where muted starts as true?
 
-dynamic_buttons = []
 
 #define functions 
 def switch():
+  global muted
   t = "unmuted" #default value
   c = "green" #default value
   if (app_enable): #only do anything if enabled
     mic.mic_mute_toggle()
-    global muted
+    draw_test_label(app_enable)
+
     muted = not muted
     if (muted):
         t = "muted"
@@ -34,17 +35,31 @@ def switch():
         c= 'green'
   draw_mute_label(t,c)
 
-def enable_toggle():
+def disable():
+  global app_enable
+  app_enable = False
+  e = "disabled"
+  mouthd.clear_capture()
+  draw_enable_label(e)
+
+def enable():
     global app_enable
-    app_enable = not app_enable
-    if (app_enable):
-      e = "enabled"
-    else:
-      e = "disabled"
+    app_enable = True
+    e = "enabled"
+    mouthd.initialize_capture()
     draw_enable_label(e)
+  
+def enable_toggle():
+  if (app_enable):
+    disable()
+  else:
+    enable()
 
 def poll_webcam():
-    print(mouthd.get_mouth_state())
+    if (app_enable):
+      m_open = mouthd.get_mouth_state()
+      if ((muted and m_open) or ((not muted) and (not m_open))):
+        switch()
     root.after(webcam_poll_delay, poll_webcam)  # reschedule event 
 
 def draw_mute_label(t,c):
@@ -55,16 +70,26 @@ def draw_enable_label(e):
     enable_label = tk.Label(root, text= e, fg="black", font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 150, window=enable_label)
 
+def draw_test_label(test):
+    test_label = tk.Label(root, text= test, fg="blue", font=('helvetica', 12, 'bold'))
+    canvas1.create_window(200, 200, window=test_label)
+
+
+
 #define app
 root= tk.Tk()
 canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
-  
+
+##todo how to check if system muted already?
+
+disable()
+
 #define widgets   
 mute_button = tk.Button(text='Toggle Mute',command=switch, bg='brown',fg='white')
 enable_button = tk.Button(text='enable app',command=enable_toggle, bg='brown',fg='white')
 draw_mute_label("unmuted","green")
-draw_enable_label("enabled")
+draw_enable_label("disabled")
 
 #add widgets to canvas 
 canvas1.create_window(50, 50, window=mute_button)
@@ -72,5 +97,5 @@ canvas1.create_window(150, 50, window=enable_button)
 
 
 #mouthd.initialize_capture()
-#root.after(webcam_poll_delay, poll_webcam)
+root.after(webcam_poll_delay, poll_webcam)
 root.mainloop()
--- mic_control.py
+++ mic_control.py
@@ -11,3 +11,4 @@ APPCOMMAND_MICROPHONE_VOLUME_MUTE = 0x180000
 def mic_mute_toggle():
   hwnd_active = win32gui.GetForegroundWindow()
   win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None, APPCOMMAND_MICROPHONE_VOLUME_MUTE)
+
